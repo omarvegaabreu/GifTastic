4839be3fad6096e0349f0d2a48eb7ea3ee6d5de8
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-07-14 19:57:02 -0400

    app is not working

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,14 +18,48 @@ $(document).ready(function() {
     }
   }
   //Create a function that will populate the images from the giphy API
-  $(
-    document.onabort("click", ".animals-button", function() {
+  $(document).on("click", ".animals-button", function() {
       $("#images").empty();
 
       $(".animals-button").removeClass("active");
       $(this).addClass("active");
 
       let type = $(this).attr("data-type");
+      let queryURL =
+        "http:api.giphy.com/v1/gifs/search?q=" +
+        type +
+        "api_key=r3bhcK4a4SJx6g1cuDCKj8yBu3jnBBJ8&limit=10";
+
+      //ajax call
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        let results = response.data;
+
+        for (let i = 0; i < results.length; i++) {
+          //variables
+          let animalsDiv = $(`<div class=\"animals-item\">`);
+          let rating = results[i].rating;
+          let p = $(`<p>`).text("Rating: " + rating);
+          let animated = results[i].images.fixed_height.url;
+          let still = results[i].images.fixed_height.url;
+          let animalsImage = $("<img>");
+
+          //functions
+          animalsImage.attr("src", still);
+          animalsImage.attr("data-still", still);
+          animalsImage.attr("data-animate", animated);
+          animalsImage.attr("data-state", "still");
+
+          animalsImage.addClass("animals-image");
+
+          animalsDiv.append(a);
+          animalsDiv.append(animalsImage);
+
+          $("#images").append(animalsDiv);
+        }
+      });
     })
   );
 });
